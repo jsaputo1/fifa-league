765f64f4a8f00964fa6c2f07a2f406cb25004848
John Saputo <John@John-MBP.local>
2020-03-24 19:27:43 -0400

    style: removed link from team name

--- themes/fifa-league/front-page.php
+++ themes/fifa-league/front-page.php
@@ -31,7 +31,7 @@
 
             <tbody>
                 <tr>
-                    <td class="team-name"><a href="<?php the_permalink()?>"><?php the_post_thumbnail() ?><?php the_title()?></a></td>
+                    <td class="team-name"><?php the_post_thumbnail() ?><?php the_title()?></a></td>
                     <td>
                     <?php echo
                     (get_post_meta( get_the_ID(), '_fifa_w', true )+get_post_meta( get_the_ID(), '_fifa_d', true )+get_post_meta( get_the_ID(), '_fifa_l', true ));?>
@@ -83,7 +83,7 @@
 
             <tbody>
                 <tr>
-                    <td class="team-name"><a href="<?php the_permalink()?>"><?php the_post_thumbnail() ?><?php the_title()?></a></td>
+                    <td class="team-name"><?php the_post_thumbnail() ?><?php the_title()?></a></td>
                     <td>
                     <?php echo
                     (get_post_meta( get_the_ID(), '_fifa_w', true )+get_post_meta( get_the_ID(), '_fifa_d', true )+get_post_meta( get_the_ID(), '_fifa_l', true ));?>
@@ -111,162 +111,8 @@
 </section>
 
 <!-- Fixtures -->
-
 <section class="fixture-page">
-
-    <div class="fixture-menu">
-        <ul>
-            <li class="menu-click-a" id="selectedGroup">Group A</li>
-            <li class="dash">|</li>
-            <li class="menu-click-b">Group B</li>
-        </ul>   
-    </div>
-
-    <!-- Group A Fixtures -->
-
-    <div class="group-a-fixtures">
-
-            <?php $args = array( 'post_type' => 'post', 
-                    'order' => 'ASC',
-                    'cat' => '2', 
-                    'cat' => '7', 
-                    'cat' => '8',
-                    'cat' => '12',
-       
-                    'posts_per_page' => '999'
-                    ); $product_posts = get_posts( $args );?>
-                    <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-
-            <div class="matchup">
-                <div class="teams">
-                    <p class="team-name1-fixtures">
-                    <img src="
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_t1img', true ));?>
-                    ">
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_team1', true ))?></p>
-                    <p class="team-name1-fixtures">
-                    <img src="<?php echo (get_post_meta( get_the_ID(), '_fifa_t2img', true ));?>">
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_team2', true ))?></p>
-                </div>
-                <div class="score">
-
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_team1Score', true ))?></p>
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_team2Score', true ))?></p>
-                </div>
-                <div class="final">
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_final', true ))?></p>
-                </div>
-            </div>
-            <hr>
-        <?php endforeach?>      
-
-        <?php $args = array( 'post_type' => 'post', 
-                    'order' => 'ASC',
-                    'cat' => '2', 
-                    'cat' => '7', 
-                    'cat' => '8',
-                    'cat' => '13',
-                    'posts_per_page' => '999'
-                    ); $product_posts = get_posts( $args );?>
-                    <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-
-            <div class="matchup" id="pending">
-                <div class="teams">
-                    <p class="team-name1-fixtures">
-                    <img src="
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_t1img', true ));?>
-                    ">
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_team1', true ))?></p>
-                    <p class="team-name1-fixtures">
-                    <img src="<?php echo (get_post_meta( get_the_ID(), '_fifa_t2img', true ));?>">
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_team2', true ))?></p>                
-                </div>
-                <div class="score">
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_team1Score', true ))?></p>
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_team2Score', true ))?></p>
-                </div>
-            
-                <div class="final">
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_final', true ))?></p>
-                </div>         
-            </div>
-            <hr>
-            <?php endforeach?>
-    </div>
-    <!-- Group B Fixtures -->
-
-    <div class="group-b-fixtures">
-
-            <?php $args = array( 'post_type' => 'post', 
-                    'order' => 'ASC',
-                    'cat' => '2', 
-                    'cat' => '7', 
-                    'cat' => '8',
-                    'cat' => '10',
-       
-                    'posts_per_page' => '999'
-                    ); $product_posts = get_posts( $args );?>
-                    <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-
-            <div class="matchup">
-                <div class="teams">
-                    <p class="team-name1-fixtures">
-                    <img src="
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_t1img', true ));?>
-                    ">
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_team1', true ))?></p>
-                    <p class="team-name1-fixtures">
-                    <img src="<?php echo (get_post_meta( get_the_ID(), '_fifa_t2img', true ));?>">
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_team2', true ))?></p>
-                </div>
-                <div class="score">
-
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_team1Score', true ))?></p>
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_team2Score', true ))?></p>
-                </div>
-                <div class="final">
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_final', true ))?></p>
-                </div>
-            </div>
-            <hr>
-        <?php endforeach?>      
-
-        <?php $args = array( 'post_type' => 'post', 
-                    'order' => 'ASC',
-                    'cat' => '2', 
-                    'cat' => '7', 
-                    'cat' => '8',
-                    'cat' => '11',
-                    'posts_per_page' => '999'
-                    ); $product_posts = get_posts( $args );?>
-                    <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-
-            <div class="matchup" id="pending">
-                <div class="teams">
-                    <p class="team-name1-fixtures">
-                    <img src="
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_t1img', true ));?>
-                    ">
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_team1', true ))?></p>
-                    <p class="team-name1-fixtures">
-                    <img src="<?php echo (get_post_meta( get_the_ID(), '_fifa_t2img', true ));?>">
-                    <?php echo (get_post_meta( get_the_ID(), '_fifa_team2', true ))?></p>                
-                </div>
-                <div class="score">
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_team1Score', true ))?></p>
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_team2Score', true ))?></p>
-                </div>
-            
-                <div class="final">
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_final', true ))?></p>
-                </div>         
-            </div>
-            <hr>
-            <?php endforeach?>
-    </div>
-  
+    <?php include ('page-fixtures.php');?>
 </section>
-
-
 
 <?php get_footer();?>
