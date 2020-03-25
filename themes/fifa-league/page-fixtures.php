ef425882b594681a8bb8c884dc61b8ddfa2ace29
John Saputo <John@John-MBP.local>
2020-03-25 04:09:50 -0400

    fix: fixed duplicate code error in group b section

--- themes/fifa-league/page-fixtures.php
+++ themes/fifa-league/page-fixtures.php
@@ -75,6 +75,8 @@
                     ); $product_posts = get_posts( $args );?>
                     <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
 
+                    
+
         <div class="matchup" id="pending">
             <div class="teams">
                 <p class="team-name1-fixtures">
@@ -112,35 +114,42 @@
                     ); $product_posts = get_posts( $args );?>
                     <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
 
-            <div class="matchup">
-                <div class="teams">       
-                    <?php if 
-                        ($team1Score < $team2Score) : 
-                        echo "<p class='team-name1-fixtures' id='loser'><img src='$team1img; ?>'> $team1 </p>";
-                        else : 
-                        echo "<p class='team-name1-fixtures' id=''> <img src='$team1img; ?>'> $team1 </p>";
-                    endif; ?>                
-                    <?php if ($team2Score < $team1Score) :
-                        echo "<p class='team-name1-fixtures' id='loser'><img src='$team2img; ?>'> $team2 </p>";
-                        else : echo "<p class='team-name1-fixtures' id=''> <img src='$team2img; ?>'> $team2 </p>";
-                    endif; ?> 
-                </div>
-                <div class="score">
+                    <?php $team1 = (get_post_meta( get_the_ID(), '_fifa_team1', true )) ?>
+        <?php $team2 = (get_post_meta( get_the_ID(), '_fifa_team2', true )) ?>
+        <?php $team1img = (get_post_meta( get_the_ID(), '_fifa_t1img', true ));?>
+        <?php $team2img = (get_post_meta( get_the_ID(), '_fifa_t2img', true ));?>
+        <?php $team1Score = (get_post_meta( get_the_ID(), '_fifa_team1Score', true ))?>
+        <?php $team2Score = (get_post_meta( get_the_ID(), '_fifa_team2Score', true ))?>
+
+        <div class="matchup">
+            <div class="teams">       
                 <?php if 
-                    ($team1Score < $team2Score) : echo "<p id='loser'>$team1Score </p>";
-                    else : echo "<p id=''>$team1Score </p>";
-                endif; ?>
-                <?php
-                    if 
-                    ($team2Score < $team1Score) : echo "<p id='loser'>$team2Score </p>";
-                    else : echo "<p id=''>$team2Score </p>";
-                endif; ?>
-                </div>
-                <div class="final">
-                    <p><?php echo (get_post_meta( get_the_ID(), '_fifa_final', true ))?></p>
-                </div>
+                    ($team1Score < $team2Score) : 
+                    echo "<p class='team-name1-fixtures' id='loser'><img src='$team1img; ?>'> $team1 </p>";
+                    else : 
+                    echo "<p class='team-name1-fixtures' id=''> <img src='$team1img; ?>'> $team1 </p>";
+                endif; ?>                
+                <?php if ($team2Score < $team1Score) :
+                    echo "<p class='team-name1-fixtures' id='loser'><img src='$team2img; ?>'> $team2 </p>";
+                    else : echo "<p class='team-name1-fixtures' id=''> <img src='$team2img; ?>'> $team2 </p>";
+                endif; ?> 
             </div>
-            <hr>
+            <div class="score">
+            <?php if 
+                ($team1Score < $team2Score) : echo "<p id='loser'>$team1Score </p>";
+                else : echo "<p id=''>$team1Score </p>";
+            endif; ?>
+            <?php
+                if 
+                ($team2Score < $team1Score) : echo "<p id='loser'>$team2Score </p>";
+                else : echo "<p id=''>$team2Score </p>";
+            endif; ?>
+            </div>
+            <div class="final">
+                <p><?php echo (get_post_meta( get_the_ID(), '_fifa_final', true ))?></p>
+            </div>
+        </div>
+        <hr>
         <?php endforeach?>      
 
         <?php $args = array( 'post_type' => 'post', 
